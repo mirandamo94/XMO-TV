775f59485e67cbc65192792d7e0a9a652c3da156
Miranda Mo <moxmo@loop-depaulsecure-252-131.depaulsecure-student.depaul.edu>
2018-02-27 11:35:02 -0600

    Initial Commit

---
+++ XMO TV/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  XMO TV
+//
+//  Created by Miranda Mo on 2/27/18.
+//  Copyright © 2018 Miranda Mo. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ XMO TV/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  XMO TV
+//
+//  Created by Miranda Mo on 2/27/18.
+//  Copyright © 2018 Miranda Mo. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
